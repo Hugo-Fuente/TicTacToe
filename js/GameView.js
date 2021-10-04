d4e1ee7b5e2d4d88c86d8c7462ba49017646b6ec
Hugo-Fuente <h172386@dac.unicamp.br>
2021-10-04 11:01:03 -0300

    header css done

--- js/GameView.js
+++ js/GameView.js
@@ -2,14 +2,14 @@ export default class GameView {
     constructor(root) {
         this.root = root;
         this.root.innerHTML = `
-            <div class ="header">
+            <div class="header">
                 <div class="header__turn">
                     X's turn
                 </div>
-                <div class"header__status">
+                <div class="header__status">
                     In Progress
                 </div>
-                <button type="button" class"header__restart">
+                <button type="button" class="header__restart">
                     <i class="material-icons">refresh</i>
                 </button>
             </div>
--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ import GameView from "./GameView.js";
 let game = new Game();
 let gameView = new GameView(document.getElementById("app"));
 
+
+/*------------------------------------------------------------------------------------------*/
 /*
 // Testing the turns
 console.log(game.turn);
